112d5b5e87911cb29dc830146e57868f0dd41c34
Dani Lyn <dlyn1110@gmail.com>
2018-06-21 17:55:50 +0000

    edits in cli

---
+++ lib/happy_hour/bars.rb
@@ -0,0 +1,14 @@
+class HappyHour::Bar
+
+attr_accessor :name, :neighborhood, :address, :description
+
+  def initialize
+    @name = name
+    @neighborhood = neighborhood
+    @address = address
+    @description = description
+  end
+
+  
+
+end
--- lib/happy_hour/cli.rb
+++ lib/happy_hour/cli.rb
@@ -2,10 +2,30 @@ class HappyHour::CLI
 
   def call
     greeting
+    options
   end
 
  def greeting
    puts "Welcome To the Best Happy Hour Bar Source That Brooklyn Has To Offer!!!"
+   puts ""
+   puts "Please choose numbers 1-7 for desired neighborhood:"
+   puts ""
  end
 
-end
+ def options
+   puts "1. Bed-Stuy"
+   puts "2. Bushwick"
+   puts "3. Cobble Hill/Boerum Hill/Carroll Gardens"
+   puts "4. Dumbo/Brooklyn Heights"
+   puts "5. Greenpoint"
+   puts "6. Park Slope"
+   puts "7. Williamsburg"
+   puts ""
+
+  end
+
+   def more_info
+
+     input = gets.strip.downcase
+
+  end
