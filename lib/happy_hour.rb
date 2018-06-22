846a00d41b20d599049f19309cdb23031d90248c
Dani Lyn <dlyn1110@gmail.com>
2018-06-22 00:03:25 +0000

    add files to environment

--- lib/happy_hour.rb
+++ lib/happy_hour.rb
@@ -1,9 +1,8 @@
-require "happy_hour/version"
+require_relative './lib/happy_hour/version'
+require_relative './happy_hour/cli'
+require_relative './happy_hour/bars'
 
 
 module HappyHour
   # Your code goes here...
 end
-
-
-require_relative './happy_hour/cli'
