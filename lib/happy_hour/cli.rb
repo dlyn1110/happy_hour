492ffab1df77c019fe03fdf58d965fa9252d5102
Dani Lyn <dlyn1110@gmail.com>
2018-06-23 23:48:27 +0000

    added cli functions

--- lib/happy_hour/cli.rb
+++ lib/happy_hour/cli.rb
@@ -2,19 +2,22 @@ class HappyHour::CLI
 
   def call
     greeting
-    options
+    main_menu
     more_info
     goodbye
   end
 
  def greeting
+   puts ""
    puts "Welcome To the Best Happy Hour Bar Source That Brooklyn Has To Offer!!!"
    puts ""
-   puts "Please choose numbers 1-7 for desired neighborhood:"
-   puts ""
+  #  puts "Please choose numbers 1-7 for desired neighborhood:"
+  #  puts ""
  end
 
- def options
+ def main_menu
+   puts "Please choose numbers 1-7 for desired neighborhood:"
+   puts ""
    puts "1. Bed-Stuy"
    puts "2. Bushwick"
    puts "3. Cobble Hill/Boerum Hill/Carroll Gardens"
@@ -28,8 +31,12 @@ class HappyHour::CLI
 
    def more_info
 
-     input = gets.strip.downcase
-       if input == "1"
+     input = nil
+      while input != 'exit'
+       puts ""
+       input = gets.strip.downcase
+
+       if input =="1"
          puts " List of Bed-Stuy Bars"
        elsif input == "2"
          puts "List of Bushwick Bars"
@@ -43,10 +50,12 @@ class HappyHour::CLI
          puts "List of Park Slope Bars"
        elsif input == "7"
          puts "List of Williamsburg Bars"
-       elsif input == "exit"
-         puts "Please choose number from list"
-      end
-      
+       elsif input == "menu"
+         main_menu
+       else input == 'exit'
+         break
+    end
+   end
   end
 
     def goodbye
