11a11353643cafddf2c3a9a28c42f8000ff10fcc
Dani Lyn <dlyn1110@gmail.com>
2018-06-24 21:03:15 +0000

    small edit to bar file

--- lib/happy_hour/bars.rb
+++ lib/happy_hour/bars.rb
@@ -1,13 +1,12 @@
-
+require 'pry'
 class HappyHour::Bar
 
 attr_accessor :name, :description
 
   @@bars = []
 
-  def initialize(name:, description:)
-    @name = name
-    @description = description
+  def initialize
+    @@bars << self
   end
 
   def self.all
@@ -16,11 +15,9 @@ attr_accessor :name, :description
 
   def self.scrape_bars
     doc = Nokogiri::HTML(open('https://www.amny.com/eat-and-drink/brooklyn-s-best-bar-deals-happy-hours-free-snacks-games-and-more-1.11756526'))
-    doc.css('.mediaCell').collect do |bar|
-      {
-    name: doc.css('h2').text,
-    bar_description: doc.css('p')[4].text
-       }
+    doc.css('.mediaCell').each do |bar|
+      name = doc.css('h2').text,
+      description = doc.css('p')[4].text
     end
   end
 end
