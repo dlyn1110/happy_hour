87d05ed1e6f542470af37919a6861fdad78fe07b
Dani Lyn <dlyn1110@gmail.com>
2018-06-24 15:37:43 +0000

    install gem-release

--- lib/happy_hour/bars.rb
+++ lib/happy_hour/bars.rb
@@ -2,13 +2,13 @@
 class HappyHour::Bar
 
 attr_accessor :name, :neighborhood, :address, :bar_description
-  
+
   @@bars = []
-   
+
   def initialize
     @@bars << self
   end
-  
+
   def self.all
     @@bars
   end
@@ -16,7 +16,7 @@ attr_accessor :name, :neighborhood, :address, :bar_description
   def self.scrape_bar_site
     doc = Nokogiri::HTML(open('https://www.theinfatuation.com/new-york/guides/the-best-brooklyn-happy-hours'))
     doc.css('.spot-block__title').each do |bar|
-  
+    
     end
   end
 end
