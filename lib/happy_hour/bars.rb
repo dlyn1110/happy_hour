1f72ad4ef3919f3b5e8fc8b646a295d78a3aa7c3
dlyn1110 <31673318+dlyn1110@users.noreply.github.com>
2018-06-24 11:14:14 -0400

    Update bars.rb

--- lib/happy_hour/bars.rb
+++ lib/happy_hour/bars.rb
@@ -10,7 +10,7 @@ attr_accessor :name, :neighborhood, :address, :bar_description
     @bar_description = bar_description
   end
 
-  def scrape
+  def self.scrape_bar_site
     doc = Nokogiri::HTML(open('https://www.theinfatuation.com/new-york/guides/the-best-brooklyn-happy-hours'))
     doc.css('.spot-block__title').each do |bar|
   
